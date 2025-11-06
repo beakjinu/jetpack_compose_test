b246fba8092559dd5863a3a95c8196a31cc277a1
beakjinu <dighwlsdn96@naver.com>
2025-11-07 00:34:15 +0900

    lazyColumn

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,21 @@
 package com.example.myapplication
 
-import androidx.compose.ui.graphics.Color
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -28,23 +24,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Box(modifier = Modifier.background(color = Color.Green)
-                .fillMaxWidth()
-                .height(200.dp)
-                , contentAlignment = Alignment.TopCenter
-            )
-            {
-                Text("Hello")
-                Box(modifier = Modifier.fillMaxSize().padding(40.dp),
-                    contentAlignment = Alignment.BottomEnd
-                ) {
-                    Text("world")
+            val scrollState = rememberScrollState()
+
+            Column(
+                modifier = Modifier
+                    .systemBarsPadding()
+                    .verticalScroll(scrollState)
+            ){
+
+                Text("1번 텍스트")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("2번 텍스트")
+                for (i in 1..50){
+                    Text("글씨 ${i}")
                 }
-            }
-        }
-        setContent {
-            Box(modifier = Modifier.background(color = Color.Gray)){
-                Text("Beakjinu")
             }
         }
     }
