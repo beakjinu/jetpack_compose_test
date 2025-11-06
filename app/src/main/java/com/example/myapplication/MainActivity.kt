7186374cb486a166535695fdef27a24d337ffc13
beakjinu <dighwlsdn96@naver.com>
2025-11-06 23:49:36 +0900

    BOX

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,46 +6,46 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Surface(color = MaterialTheme.colorScheme.background ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp))
-                    {
-                        Row {
-                            Text("hello")
-                            Spacer(Modifier.width(16.dp))
-                            greeting("하이")
-                        }
-                    }
+            Box(modifier = Modifier.background(color = Color.Green)
+                .fillMaxWidth()
+                .height(200.dp)
+                , contentAlignment = Alignment.TopCenter
+            )
+            {
+                Text("Hello")
+                Box(modifier = Modifier.fillMaxSize().padding(40.dp),
+                    contentAlignment = Alignment.BottomEnd
+                ) {
+                    Text("world")
                 }
             }
         }
-    }
-
-    @Composable
-    fun greeting(name : String){
-        Text(text = "인사 드립니다. ${name}")
+        setContent {
+            Box(modifier = Modifier.background(color = Color.Gray)){
+                Text("Beakjinu")
+            }
+        }
     }
 }
